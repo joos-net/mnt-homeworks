888e17df7154820c7933ab852bb41aca2b195575
Jo Os <pirtank@gmail.com>
2024-02-19 11:49:44 +0300

    add img

---
+++ 08-ansible-01-base/playbook/bash.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#create docker
+docker run -d --name centos7 pycontribs/centos:7 sleep 10000
+docker run -d --name ubuntu pycontribs/ubuntu sleep 10000
+docker run -d --name fedora pycontribs/fedora sleep 10000
+
+#start ansible
+ansible-playbook -i inventory/prod.yml site.yml --ask-vault-pass
+
+#rm docker
+docker rm -f centos7 ubuntu fedora
